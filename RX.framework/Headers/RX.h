ef5f772c9ece2cbf2aa46f30b54a9de233a4859a
clinicdr25i3rabah-collab <clinicdr25i3rabah@gmail.com>
2026-09-09 23:10:44 +0300

    Add files with proper LFS tracking

---
+++ RX.framework/Headers/RX.h
@@ -0,0 +1,18 @@
+//
+//  RX.h
+//  RX
+//
+//  Created by Sniper Team on 07/04/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RX.
+FOUNDATION_EXPORT double RXVersionNumber;
+
+//! Project version string for RX.
+FOUNDATION_EXPORT const unsigned char RXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RX/PublicHeader.h>
+
+
